9a1bac6a54def6ab7d3fc4bc292eaab28bef39e7
xKarol <karolpiskorz8@gmail.com>
2022-02-25 13:32:49 +0100

    Improved articles section responsive

--- src/components/latest-articles/article.js
+++ src/components/latest-articles/article.js
@@ -1,18 +1,18 @@
 function Article({ author, heading, text, img }) {
   return (
     <div className="col">
-      <article className="card border-0 rounded">
+      <article className="card border-0 rounded h-100">
         <img
           src={img}
           className="card-img-top latest-articles__image"
           alt="Receive money in any currency with no fees"
         />
-        <div className="card-body p-5 text-grayish-blue">
+        <div className="card-body p-5 p-md-3 text-grayish-blue">
           <small>By {author}</small>
           <h4 className="text-dark fs-2 mt-3 latest-articles__heading">
             {heading}
           </h4>
-          <p>{text}</p>
+          <p className="latest-articles__text">{text}</p>
         </div>
       </article>
     </div>
