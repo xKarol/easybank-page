fe170a7ee8e1a8d9a53562a7b2e0a0b719000824
xKarol <karolpiskorz8@gmail.com>
2022-02-26 09:36:25 +0100

    Disabled scroll in hamburger menu, Fixed hero text max width

--- src/components/header/hamburger.js
+++ src/components/header/hamburger.js
@@ -1,4 +1,15 @@
+import { useEffect, useState } from "react";
+
 function Hamburger() {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    if (!active) return;
+    window.scrollTo(0, 0);
+    document.body.classList.add("no-scroll");
+    return () => document.body.classList.remove("no-scroll");
+  }, [active]);
+
   return (
     <button
       className="navbar-toggler border-0 header__navbar__hamburger collapsed"
@@ -8,6 +19,7 @@ function Hamburger() {
       aria-controls="navbarNav"
       aria-expanded="false"
       aria-label="Toggle navigation"
+      onClick={() => setActive(!active)}
     >
       <span className="navbar-toggler-icon"></span>
     </button>
